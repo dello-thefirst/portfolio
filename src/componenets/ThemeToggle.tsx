324164d073d1a2896e06bb728f06fd295656a077
victorfaruna <farunavictor043@gmail.com>
2024-08-21 12:22:33 +0100

    jowe

--- src/componenets/ThemeToggle.tsx
+++ src/componenets/ThemeToggle.tsx
@@ -2,29 +2,51 @@
 import React, { useEffect, useState } from "react";
 
 const ThemeToggle = () => {
-  const [darkMode, setDarkMode] = useState(true);
+  const handleThemeToggle = () => {
+    if (typeof window !== undefined) {
+      const theme = localStorage.getItem("theme");
+      if (theme === "light") {
+        document.body.classList.replace("light", "dark");
+        localStorage.setItem("theme", "dark");
+      } else {
+        document.body.classList.replace("dark", "light");
+        localStorage.setItem("theme", "light");
+      }
+    }
+  };
+
   useEffect(() => {
     const theme = localStorage.getItem("theme");
     if (theme) {
-      setDarkMode(theme === "dark");
-    }
-  }, []);
-  useEffect(() => {
-    const theme = localStorage.getItem("theme");
-    if (darkMode) {
-      document.body.classList.remove("light");
+      if (theme === "light") {
+        document.body.classList.add("light");
+        document.body.classList.remove("dark");
+        localStorage.setItem("theme", "light");
+      } else {
+        document.body.classList.add("dark");
+        document.body.classList.remove("light");
+        localStorage.setItem("theme", "dark");
+      }
+    } else {
       document.body.classList.add("dark");
       localStorage.setItem("theme", "dark");
-    } else {
-      document.body.classList.remove("dark");
-      document.body.classList.add("light");
-      localStorage.setItem("theme", "light");
     }
-  }, [darkMode]);
+  }, []);
+
+  //   useEffect(() => {
+  //     const theme = localStorage.getItem("theme");
+  //     if (darkMode) {
+  //       document.body.classList.replace("light", "dark");
+  //       localStorage.setItem("theme", "dark");
+  //     } else {
+  //       document.body.classList.replace("dark", "light");
+  //       localStorage.setItem("theme", "light");
+  //     }
+  //   }, [darkMode]);
   return (
     <div
       className="cursor-pointer text-theme-color"
-      onClick={() => setDarkMode(!darkMode)}
+      onClick={() => handleThemeToggle()}
     >
       <i className="fa-light fa-sun-dust text-"></i>
     </div>
