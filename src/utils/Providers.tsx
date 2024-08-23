73b4ee09b3dd58d78cc59eaed336d844364d2ed4
victorfaruna <farunavictor043@gmail.com>
2024-08-23 10:16:48 +0100

    h asuqkhl

--- src/utils/Providers.tsx
+++ src/utils/Providers.tsx
@@ -1,6 +1,7 @@
 "use client";
 import Header from "@/src/componenets/Header";
 import { Poppins } from "next/font/google";
+import { useEffect, useState } from "react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700"],
@@ -9,10 +10,13 @@ const poppins = Poppins({
   display: "swap",
 });
 export default function Providers({ children }: { children: React.ReactNode }) {
-  const theme =
-    typeof window !== undefined && localStorage
-      ? localStorage.getItem("theme")
-      : "dark";
+  const [theme, setTheme] = useState("dark");
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setTheme(savedTheme);
+    }
+  }, []);
   return (
     <>
       <body className={`bg-main ${theme} ${poppins.className}`}>
