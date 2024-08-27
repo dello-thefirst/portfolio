da94a3e421e5a82e06e1d9acb33969e4031b83d7
victorfaruna <farunavictor043@gmail.com>
2024-08-27 21:05:45 +0100

    jscqibwi

--- src/componenets/Services.tsx
+++ src/componenets/Services.tsx
@@ -9,45 +9,50 @@ const Services = () => {
   return (
     <div
       ref={ref}
-      className={`w-full flex gap-5 mb-10 sm:flex-col ${
+      className={`w-full py-[2%] sm:py-[10%] ${
         inView ? "revealTrans" : "hiddenTrans"
       }`}
     >
-      <div
-        className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
-      >
-        <i className="text-[50px] text-color-2 fa-solid fa-code"></i>
-        <p className="text-color-1">Fullstack Development</p>
+      <p className="text-[20px] text-color-2 font-medium mb-8">
+        Services I Offer<span className="text-color-1">.</span>
+      </p>
+      <div className="flex gap-5 sm:flex-col">
+        <div
+          className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
+        >
+          <i className="text-[50px] text-color-2 fa-solid fa-code"></i>
+          <p className="text-color-1">Fullstack Development</p>
 
-        <p className="text-color-3 text-[12px] text">
-          I create end-to-end solutions, handling both frontend and backend
-          development with React, Next.js, Python, and PHP to deliver complete,
-          efficient web and mobile applications.
-        </p>
-      </div>
-      <div
-        className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
-      >
-        <i className="text-[50px] text-color-2 fa-regular fa-mobile"></i>
-        <p className="text-color-1">Mobile App Development</p>
+          <p className="text-color-3 text-[12px] text">
+            I create end-to-end solutions, handling both frontend and backend
+            development with React, Next.js, Python, and PHP to deliver
+            complete, efficient web and mobile applications.
+          </p>
+        </div>
+        <div
+          className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
+        >
+          <i className="text-[50px] text-color-2 fa-regular fa-mobile"></i>
+          <p className="text-color-1">Mobile App Development</p>
 
-        <p className="text-color-3 text-[12px]">
-          I build cross-platform mobile applications using React Native,
-          delivering smooth, responsive user experiences for both iOS and
-          Android devices.
-        </p>
-      </div>
-      <div
-        className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
-      >
-        <i className="text-[50px] text-color-2 fa-regular fa-pen-nib"></i>
-        <p className="text-color-1">Graphic Design</p>
+          <p className="text-color-3 text-[12px]">
+            I build cross-platform mobile applications using React Native,
+            delivering smooth, responsive user experiences for both iOS and
+            Android devices.
+          </p>
+        </div>
+        <div
+          className={`w-full h-auto rounded-lg bg-color-1/10 p-4 flex flex-col gap-4 overflow-hidden`}
+        >
+          <i className="text-[50px] text-color-2 fa-regular fa-pen-nib"></i>
+          <p className="text-color-1">Graphic Design</p>
 
-        <p className="text-color-3 text-[12px]">
-          I craft visually engaging designs, blending creativity with technical
-          precision to produce compelling graphics that enhance user experiences
-          and brand identity.
-        </p>
+          <p className="text-color-3 text-[12px]">
+            I craft visually engaging designs, blending creativity with
+            technical precision to produce compelling graphics that enhance user
+            experiences and brand identity.
+          </p>
+        </div>
       </div>
     </div>
   );
